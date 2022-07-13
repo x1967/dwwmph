73578949c2518ef0e7558222e513e0ed53335436
herve1967 <93819784+herve1967@users.noreply.github.com>
2022-07-13 16:40:16 +0200

    Add files via upload

---
+++ PROJETs/TestLibrarycopie2/accueil.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+<body class="body-accueil">
+
+    <form action="index.php">
+        
+        <input type="submit" name="btn-accueil" class="btn-accueil" value="Quitter"> <input class="btn-accueil" type="submit" name="btn-accueil" value="Connexion" onclick="return action='connexions.php'"/> <input class="btn-accueil" type="submit" name="btn-accueil" value="Inscription" onclick="return action='inscriptions.php'">
+    </form>
+    <div class="div-accueil">
+        <h1 class="h1-accueil">THE FANTASY'S BOOK</h1>
+    </div>
+    <h2 class="h2-accueil">TOP LIVRE DU MOIS</h2>
+    <div class="pic-ctn">
+        <div><img src="../Test Library copie/Images/livre1.jpeg" alt="" class="pic">
+            <p class="p-accueil-slide">Test Image 2</p>
+        </div>
+        <div><img src="../Test Library copie/Images/livre2.jpeg" alt="" class="pic">
+            <p class="p-accueil-slide">Test Image 2</p> 
+        </div>
+        <div><img src="../Test Library copie/Images/livre3.jpeg" alt="" class="pic">
+            <p class="p-accueil-slide">"Un homme est abattu de trois coups de feu à son domicile. Lorsque la police arrive sur place, elle trouve sa femme, Evie, enceinte de cinq mois, l'arme à la main. Celle-ci n'est pas une inconnue pour l'enquêtrice D. D. Warren. Accusée d'avoir tué son propre père d'un coup de fusil alors qu'elle était âgée de seize ans, elle a finalement été innocentée, la justice ayant conclu à un accident. Simple coïncidence ? Evie est-elle coupable ou victime de son passé ? Edition adaptée facile à lire : malvoyance ; fatigue visuelle ; troubles de l'apprentissage ; troubles cognitifs ; troubles DYS ; dyslexie ; dysgraphie ; TDA/H ; alphabétisation, FLE.
+"</p> 
+        </div>
+</body>
+</html>
---
+++ PROJETs/TestLibrarycopie2/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenue</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body class="body-index">
+
+  <div class="background-one">
+
+    <div class="link-container">
+
+        <form action="accueil.php" method="POST">
+          
+            <a class="link-one" href="accueil.php"><input type="submit" class="button-one" value="CLIQUE !"></a>
+        </form>
+    </div>
+  </div>
+</body>
+</html>
+
+<?php
+
+
+
+?>
